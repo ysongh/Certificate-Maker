b2d7d45af762715c48d8b2801ab995fdce58548e
You Song Hou <yousonghou@gmail.com>
2021-08-08 00:42:40 -0400

    add a function to upload a file to Slate

--- src/pages/CertificateForm.js
+++ src/pages/CertificateForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Container, Card, Form, Button } from 'semantic-ui-react';
 
+import { SLATEAPIKEY, CERTIFICATETEMPLATE_COLLECTIONID } from '../config';
+
 function CertificateForm() {
   const [name, setName] = useState('');
   const [image, setImage] = useState('');
@@ -9,6 +11,24 @@ function CertificateForm() {
   const getFile = async event => {
     const file = event.target.files[0];
     console.log(file);
+    setImage(file);
+  }
+
+  const uploadFileToSlate = async () => {
+    const url = `https://uploads.slate.host/api/public/${CERTIFICATETEMPLATE_COLLECTIONID}`;
+
+    let data = new FormData();
+    data.append("data", image);
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: SLATEAPIKEY,
+      },
+      body: data
+    });
+    const json = await response.json();
+    console.log(json);
   }
 
   return (
@@ -28,6 +48,7 @@ function CertificateForm() {
             <Button
               type='submit'
               color="black"
+              onClick={uploadFileToSlate}
             >Submit</Button>
           </Form>
         </Card.Content>
