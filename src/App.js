d746f479a384cf2335f7b0c8f6696adb7074bbd8
You Song Hou <yousonghou@gmail.com>
2021-08-08 22:22:31 -0400

    install react-router-dom and set it up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import CertificateForm from './pages/CertificateForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Certificate Maker</h1>
-      <CertificateForm />
-    </div>
+    <Router className="App">
+      <Switch>
+        <Route path="/add-certificate">
+          <CertificateForm />
+        </Route>
+        <Route path="/">
+          <h1>Certificate Maker</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
