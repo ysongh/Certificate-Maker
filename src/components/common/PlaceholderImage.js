3599c177e297c4b93f5007581a0cba11e373c2cd
You Song Hou <yousonghou@gmail.com>
2021-08-11 01:19:49 -0400

    update UI on Certificate Form and display a preview of the image upload

---
+++ src/components/common/PlaceholderImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Placeholder } from 'semantic-ui-react';
+
+const PlaceholderImage = () => (
+  <Placeholder fluid style={{ height: 400 }}>
+    <Placeholder.Image />
+  </Placeholder>
+)
+
+export default PlaceholderImage;
--- src/pages/CertificateForm.js
+++ src/pages/CertificateForm.js
@@ -1,27 +1,23 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
-import { Container, Card, Form, Button } from 'semantic-ui-react';
+import { Container, Card, Form, Image, Button } from 'semantic-ui-react';
 
 import { SLATEAPIKEY, CERTIFICATETEMPLATE_COLLECTIONID } from '../config';
 import Spinner from '../components/common/Spinner';
+import PlaceholderImage from '../components/common/PlaceholderImage';
 
 function CertificateForm({ walletAddress, contract }) {
   const history = useHistory();
 
   const [price, setPrice] = useState('');
-  const [image, setImage] = useState('');
-  const [detail, setDetail] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [imageURL, setImageURL] = useState('');
+  const [loadingImage, setLoadingImage] = useState(false);
+  const [loadingCreate, setLoadingCreate] = useState(false);
 
-  const getFile = async event => {
-    const file = event.target.files[0];
-    console.log(file);
-    setImage(file);
-  }
-
-  const uploadFileToSlate = async () => {
+  const uploadFileToSlate = async event => {
     try{
-      setLoading(true);
+      setLoadingImage(true);
+      const image = event.target.files[0];
       const url = `https://uploads.slate.host/api/public/${CERTIFICATETEMPLATE_COLLECTIONID}`;
 
       let data = new FormData();
@@ -36,17 +32,29 @@ function CertificateForm({ walletAddress, contract }) {
       });
       const json = await response.json();
       console.log(json);
-
-      await contract.methods
-        .mintCertificateTemplateNFT(json.data.cid, window.web3.utils.toWei(price, 'Ether'))
-        .send({ from: walletAddress });
-
-      setLoading(false);
-      history.push('/');
+      setImageURL(`https://slate.textile.io/ipfs/${json.data.cid}`);
+      setLoadingImage(false);
     }
     catch(err) {
       console.error(err);
-      setLoading(false);
+      setLoadingImage(false);
+    }
+  }
+
+  const createCertificateTemplate = async () => {
+    try{
+      setLoadingCreate(true);
+      const res = await contract.methods
+        .mintCertificateTemplateNFT(imageURL, window.web3.utils.toWei(price, 'Ether'))
+        .send({ from: walletAddress });
+      console.log('mintCertificateTemplateNFT', res);
+
+      history.push('/');
+      setLoadingCreate(false);
+    }
+    catch(err) {
+      console.error(err);
+      setLoadingCreate(false);
     }
   }
 
@@ -55,21 +63,27 @@ function CertificateForm({ walletAddress, contract }) {
       <Card centered style={{ width: '100%'}}>
         <Card.Content>
           <Form>
-            <Form.Field>
-              <label>Price</label>
-              <input value={price} onChange={(e) => setPrice(e.target.value)} />
-            </Form.Field>
-            <Form.Field>
-              <label>Image</label>
-              <input type="file" onChange={getFile}/>
-            </Form.Field>
-            <Form.TextArea label='Detail' value={detail} onChange={(e) => setDetail(e.target.value)} />
+            <Form.Group widths='equal'>
+              <Form.Field>
+                <label>Image</label>
+                <input type="file" onChange={uploadFileToSlate}/>
+              </Form.Field>
+              <Form.Field>
+                <label>Price (ETH)</label>
+                <input value={price} onChange={(e) => setPrice(e.target.value)} />
+              </Form.Field>
+            </Form.Group>
+
+            {loadingImage
+              ? <PlaceholderImage />
+              : imageURL && <Image src={imageURL} alt="Certificate Template" fluid />}
+            
             <Button
               type='submit'
               color="black"
-              onClick={uploadFileToSlate}
+              onClick={createCertificateTemplate}
             >Submit</Button>
-            {loading && <Spinner text="Creating..." />}
+            {loadingCreate && <Spinner text="Creating..." />}
           </Form>
         </Card.Content>
       </Card>
