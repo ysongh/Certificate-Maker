7f536ae84d461a83bd8dc50b4d51626c58248cbd
You Song Hou <yousonghou@gmail.com>
2021-08-13 23:08:58 -0400

    install cors and body-parser for the server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Server Work'));
 app.use('/api/pdf', require('./pdf'));
