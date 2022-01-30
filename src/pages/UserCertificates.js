0ef6a66a0a001f1cb37e1240889289188e4b3073
You Song Hou <yousonghou@gmail.com>
2022-01-30 00:21:30 -0500

    add a spinner in User Certificates page

--- src/pages/UserCertificates.js
+++ src/pages/UserCertificates.js
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { Container, List } from 'semantic-ui-react';
 
+import Spinner from '../components/common/Spinner';
+
 function UserCertificates({ walletAddress, contract }) {
   const [certificateURLs, setCertificateURLs] = useState([]); 
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const loadNFTs = async () => {
-      const totalSupply = await contract.methods.totalSupply().call();
+      try{
+        setLoading(true);
+        const totalSupply = await contract.methods.totalSupply().call();
 
-      for(let i = 1; i <= totalSupply; i++){
-        const tokenOwner = await contract.methods.ownerOf(i).call();
-        
-        if(tokenOwner === walletAddress){
-          let certificateCID = await contract.methods.tokenURI(i).call();
-          console.log(certificateCID);
-          const certificateURL = `https://ipfs.io/ipfs/${certificateCID}/certificate.pdf`
-          setCertificateURLs([...certificateURLs, certificateURL])
+        for(let i = 1; i <= totalSupply; i++){
+          const tokenOwner = await contract.methods.ownerOf(i).call();
+          
+          if(tokenOwner === walletAddress){
+            let certificateCID = await contract.methods.tokenURI(i).call();
+            console.log(certificateCID);
+            const certificateURL = `https://ipfs.io/ipfs/${certificateCID}/certificate.pdf`
+            setCertificateURLs([...certificateURLs, certificateURL])
+          }
         }
+        setLoading(false);
+      } catch(error) {
+        console.error(error);
+        setLoading(false);
       }
-      
     }
     if(contract) loadNFTs();
   }, [walletAddress, contract, certificateURLs])
@@ -38,6 +47,7 @@ function UserCertificates({ walletAddress, contract }) {
             </List.Item>
           ))}
       </List>
+      {loading && <Spinner text="Loading" />}
     </Container>
   )
 }
