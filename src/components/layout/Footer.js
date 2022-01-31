9180bcad1f2f8982d4680c1efcb136af9c491268
You Song Hou <yousonghou@gmail.com>
2022-01-31 02:33:37 -0500

    set the padding for Navbar to 0

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,8 +3,8 @@ import { Segment } from 'semantic-ui-react';
 
 function Footer() {
   return (
-    <Segment inverted color='purple' style={{ textAlign: 'center', padding: '1.5rem 0'}}>
-      Copyright &copy; 2021 Certificate Maker NFT
+    <Segment inverted color='purple'>
+       <p style={{ textAlign: 'center', padding: '1.5rem 0'}}>Copyright &copy; 2021 Certificate Maker NFT</p>
     </Segment>
   )
 }
