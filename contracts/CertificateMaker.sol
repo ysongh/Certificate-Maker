1f25ee340278ed08d94357f01c0c928c7d387cdb
You Song Hou <yousonghou@gmail.com>
2023-01-22 22:55:42 -0500

    update contract to use Solidity 0.8.0

--- contracts/CertificateMaker.sol
+++ contracts/CertificateMaker.sol
@@ -1,46 +1,51 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.12;
+pragma solidity ^0.8.5;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract CertificateMaker is ERC721 {
+contract CertificateMaker is ERC721URIStorage {
+  using Counters for Counters.Counter;
+  Counters.Counter public _nftsid;
+
   mapping(string => CertificateTemplate) public certificateTemplateList;
 
   struct CertificateTemplate {
     string cid;
     uint date;
     uint price;
-    address payable from;
+    address from;
   }
 
   event CertificateTemplateCreated (
     string cid,
     uint date,
     uint price,
-    address payable from
+    address from
   );
 
   event CertificateNFTCreated (
     uint tokenId,
     string cid,
     uint date,
-    address payable from,
-    address payable to
+    address from,
+    address to
   );
 
-  constructor() ERC721("Certificate Maker", "CMR")  public {}
+  constructor() ERC721("Certificate Maker", "CMR") {}
 
   function createCertificateTemplate(string memory _cid, uint _price) external {
-    certificateTemplateList[_cid] = CertificateTemplate(_cid, now, _price, msg.sender);
+    certificateTemplateList[_cid] = CertificateTemplate(_cid, block.timestamp, _price, msg.sender);
 
-    emit CertificateTemplateCreated(_cid, now, _price, msg.sender);
+    emit CertificateTemplateCreated(_cid, block.timestamp, _price, msg.sender);
   }
 
-  function mintCertificateNFT(string memory _cid, address payable _to) external {
-    uint _tokenId = totalSupply().add(1);
-    _safeMint(_to, _tokenId);
+  function mintCertificateNFT(string memory _cid, address _to) external {
+    _nftsid.increment();
+    uint _tokenId = _nftsid.current();
+    _mint(_to, _tokenId);
     _setTokenURI(_tokenId, _cid);
 
-    emit CertificateNFTCreated(_tokenId, _cid, now, msg.sender, _to);
+    emit CertificateNFTCreated(_tokenId, _cid, block.timestamp, msg.sender, _to);
   }
 }
