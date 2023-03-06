4dbe2ff9cf2d83898f41b56db3f509b4c0dc4d29
You Song Hou <ysongweb3@gmail.com>
2023-03-05 21:36:45 -0500

    update the year

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,7 +4,7 @@ import { Segment } from 'semantic-ui-react';
 function Footer() {
   return (
     <Segment inverted color='purple'>
-       <p style={{ textAlign: 'center', padding: '1.5rem 0'}}>Copyright &copy; 2021 Certificate Maker NFT</p>
+       <p style={{ textAlign: 'center', padding: '1.5rem 0'}}>Copyright &copy; 2023 Certificate Maker NFT</p>
     </Segment>
   )
 }
